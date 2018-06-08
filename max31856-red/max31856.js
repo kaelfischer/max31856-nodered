154835e793184be6d362627d607d6f6af369a6a2
Kael Fischer <kael@dork.haus>
2018-06-08 09:55:38 -0600

    Now is working with a deployed node-red freezer monitor. UI, persistance fixes.

--- max31856-red/max31856.js
+++ max31856-red/max31856.js
@@ -1,39 +1,44 @@
 module.exports = function (RED) {
     "use strict";
     const max31856 = require("max31856");
-
+    
     function max31856Node(n) {
         RED.nodes.createNode(this, n);
 
-        const node = this;
+        var node = this;
 
-        this.bus = n.bus || 0;
-        this.device = n.device || 0;
-        this.tcType = n.tcType || 0;
+        this.bus = Number(n.bus) || 0;
+        this.device = Number(n.device) || 0;
+        this.tcType = Number(n.tcType) || 0;
 
-        const MAX31856 = new max31856.MAX31856(0, 0, max31856.T_TYPE);
+	
+        const MAX31856 = new max31856.MAX31856(this.bus, this.device, this.tcType);
 
         node.on("input", function (msg) {
 
-            const faults = MAX31856.faults();
-            if (faults.length) {
-                readTemp();
+            var faults = MAX31856.faults();
+            if (! faults.length) {
+                var reading = MAX31856.tempC().toPrecision(2);
+		node.status({fill: "green", shape: "dot", text: reading});
+		msg.payload = reading;
+		node.send(msg);
             } else {
                 node.status({fill: "red", shape: "dot", text: faults.toString()});
             }
         });
-
+        
         node.on("close", function () {
             //   NOOP
         });
 
-        function readTemp() {
-            node.status({fill: "green", shape: "dot", text: "Read"});
-            node.send({
-                payload: MAX31856.tempC().precision(2)
-            });
+        function readTemp(msg) {
+	    var reading = MAX31856.tempC().toPrecision(2);
+	    node.status({fill: "green", shape: "dot", text: reading});
+	    msg.payload = reading;
+            node.send(msg);
         }
     }
-    RED.nodes.registerType("max31856-red", max31856Node());
+    
+    RED.nodes.registerType("max31856", max31856Node);
 
 };
